d2dd4c0985707d7aa72b58c8606126c44dd33567
Suleyman Demir <sulodemir.47@gmail.com>
2024-11-12 13:41:07 +0300

    Courier SDK

--- src/main/java/com/sdk/courier/exception/CourierServiceException.java
+++ src/main/java/com/sdk/courier/exception/CourierServiceException.java
@@ -3,25 +3,25 @@ package com.sdk.courier.exception;
 import lombok.Getter;
 
 /**
- * CourierServiceException is a custom exception class for handling exceptions specific to
- * the Courier Service SDK. It encapsulates error details such as HTTP status code and error messages.
+ * A custom exception class for handling errors specific to the Courier Service SDK.
+ * <p>
+ * This exception encapsulates details such as the HTTP status code and a descriptive error message,
+ * providing additional context about the failure encountered when interacting with the Courier Service API.
+ * </p>
  */
 @Getter
 public class CourierServiceException extends RuntimeException {
 
     /**
-     * -- GETTER --
-     *  Retrieves the HTTP status code associated with the exception.
-     *
-     * @return the HTTP status code.
+     * The HTTP status code returned from the Courier Service API, indicating the type of error.
      */
     private final int statusCode;
 
     /**
-     * Constructs a new CourierServiceException with a detailed message and HTTP status code.
+     * Constructs a new CourierServiceException with the specified detail message and HTTP status code.
      *
-     * @param message the detail message.
-     * @param statusCode the HTTP status code from the failed request.
+     * @param message the detail message describing the nature of the error.
+     * @param statusCode the HTTP status code from the failed request, providing additional context.
      */
     public CourierServiceException(String message, int statusCode) {
         super(message);
@@ -29,15 +29,14 @@ public class CourierServiceException extends RuntimeException {
     }
 
     /**
-     * Constructs a new CourierServiceException with a detailed message, HTTP status code, and a cause.
+     * Constructs a new CourierServiceException with the specified detail message, HTTP status code, and cause.
      *
-     * @param message the detail message.
-     * @param statusCode the HTTP status code from the failed request.
-     * @param cause the cause of the exception.
+     * @param message the detail message describing the nature of the error.
+     * @param statusCode the HTTP status code from the failed request, providing additional context.
+     * @param cause the underlying cause of the exception, which may provide further details about the failure.
      */
     public CourierServiceException(String message, int statusCode, Throwable cause) {
         super(message, cause);
         this.statusCode = statusCode;
     }
-
 }
